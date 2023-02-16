4fe6eeeb5cd2cdb9cb48e6d3e47c312c10129f62
DJUgo13 <YOUR EMAIL ON GITHUB>
2023-02-16 09:20:49 +0600

    hom

--- main.py
+++ main.py
@@ -8,52 +8,15 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 TOKEN = config('TOKEN')
 
-
 bot = Bot(TOKEN)
 db = Dispatcher(bot=bot)
 
 
-@db.message_handler(commands=['start', 'hello'])
-async def start_handler(massage: types.Message):
-    await bot.send_message(massage.from_user.id, f'привет {massage.from_user.first_name}')
-    await massage.answer('это ансфер')
-    await massage.reply(massage.from_user.first_name)
-
-
 @db.message_handler(commands=['quiz'])
-async def quiz1(massage: types.Message):
+async def quiz1(call: types.Message):
     markup = InlineKeyboardMarkup()
     button = InlineKeyboardButton('next', callback_data='button')
     markup.add(button)
-
-    ques = 'кто ты воин?'
-    answer = [
-        'Бетмен-рыцарь ночи',
-        'томас шелби из семьи острые козырьки',
-        'спанч боб:квадратные штаны',
-        'Ахилес! Сын пелея ',
-        'диктор канала "Мастерская настроения"',
-        'оптимус прайм последний прайм'
-    ]
-    await bot.send_poll(
-        chat_id=massage.from_user.id,
-        question=ques,
-        options=answer,
-        is_anonymous=False,
-        type='quiz',
-        correct_option_id=3,
-        explanation='ты ахилесс',
-        open_period=15,
-        reply_markup=markup
-    )
-
-
-@db.callback_query_handler(text='button')
-async def quiz2(call: types.CallbackQuery):
-    markup = InlineKeyboardMarkup()
-    button = InlineKeyboardButton('next', callback_data='button')
-    markup.add(button)
-
     ques = 'из кокого фильма это мем?'
     answer = [
         'бетмен',
@@ -72,14 +35,14 @@ async def quiz2(call: types.CallbackQuery):
         type='quiz',
         correct_option_id=1,
         explanation='это криминальное чтиво ты угадал',
-        open_period=15
+        open_period=15,
+        reply_markup=markup
     )
 
 
 @db.callback_query_handler(text='button')
-async def quiz3(call: types.CallbackQuery):
-
-    ques = 'из кокого аниме это фото?'
+async def quiz2(call: types.CallbackQuery):
+    ques = 'в какого аниме это фото?'
     answer = [
         'HOTD',
         'ангильские ритмы',
